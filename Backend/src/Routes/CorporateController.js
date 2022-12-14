31c4af7c68d4d846e20510535c9bd2a154d82db7
MedhatJR <99026712+MedhatJR@users.noreply.github.com>
2022-12-14 16:01:21 +0100

    Revert "Merge branch 'main' of https://github.com/MedhatJR/ACML"
    This reverts commit acdfba5a002b6167cd6740aececd08be16ee7a32, reversing
    changes made to 2fc0b6f6a6b847afdfaedf017c2960ab274b2e1b.

--- Backend/src/Routes/CorporateController.js
+++ Backend/src/Routes/CorporateController.js
@@ -2,9 +2,8 @@ const express = require("express");
 const appRouter = express.Router();
 const Course = require("../Models/Course");
 const Corporate = require("../Models/CorporateTrainee");
-const CorporateTrainee = require("../Models/CorporateTrainee");   ///////Twice????????????
+const CorporateTrainee = require("../Models/CorporateTrainee"); ///////Twice????????????
 const cors = require("cors");
-const Exams = require("../Models/Exams");
 appRouter.use(cors());
 const Instructor = require("../Models/Instructor");
 
@@ -48,10 +47,9 @@ appRouter.post("/Corporate_SelectCountry", async (req, res) => {
   );
 });
 
-
 appRouter.post("/Corporate_rateInstructor", async (req, res) => {
   Instructor.findOneAndUpdate(
-    {Email: req.body.Email},
+    { Email: req.body.Email },
     { Rating: req.body.Rating },
     { new: true },
     (error, data) => {
@@ -67,7 +65,7 @@ appRouter.post("/Corporate_rateInstructor", async (req, res) => {
 
 appRouter.post("/Corporate_rateCourse", async (req, res) => {
   Course.findOneAndUpdate(
-    {Name: req.body.Name},
+    { Name: req.body.Name },
     { Rating: req.body.Rating },
     { new: true },
     (error, data) => {
@@ -84,15 +82,6 @@ appRouter.post("/Corporate_rateCourse", async (req, res) => {
 appRouter.get("/Corporate_retrieveCourses", async (req, res) => {
   res.send(await Course.find().select(["Title", "Hours", "Rating"]));
 });
-appRouter.post("/Corporate_Login", async (req, res) => {
-  const Email = req.body.email;
-  const Password = req.body.Password;
-  Corporate.find({ Email : Email , Password: Password} ,(err,data ) => {
-  if(err){res.send(err);}
-  else { res.send("loged in");}
-  }
-  );
-});
 
 appRouter.get("/Corporate_retrieveAll", async (req, res) => {
   res.send(
@@ -108,8 +97,6 @@ appRouter.get("/Corporate_retrieveAll", async (req, res) => {
   );
 });
 
-
-
 appRouter.post("/createCorporateUser", async (req, res) => {
   const newuser = new Corporate({
     Username: req.body.Username,
@@ -120,13 +107,11 @@ appRouter.post("/createCorporateUser", async (req, res) => {
     Lastname: req.body.Lastname,
     Gender: req.body.Gender,
   });
-
   try {
     await Corporate.create(newuser);
   } catch (err) {
     console.log(err);
   }
-
   console.log("Hello");
   res.status(200).send("registration successful");
 });
@@ -145,46 +130,5 @@ appRouter.post("/Corporate_filtercourse", async (req, res) => {
     }
   );
 });
-appRouter.get("/Corporate_view_exam", async (req, res) => {
-  res.send(await Exams.find().select(["Question1", "Choice11", "Choice12","Choice13", "Choice14", "Question2","Choice21", "Choice22", "Choice23","Choice24"]));
-});
-  
 
-appRouter.post("/Corporate_ChangePassword" , async(req,res) => {
-  const OldPassword = req.body.OldPassword;
-  const NewPassword = req.body.NewPassword;
-  Corporate.findOneAndUpdate(
-    { Password : OldPassword },
-    { Password : NewPassword },
-    { new: true },
-    (error, data) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(data);
-        res.status(200).send("update done");
-      }
-    }
-  );
-  
-});
-
-appRouter.post("/Corporate_ForgotPassword" , async(req,res) => {
-  const Username = req.body.Username;
-  const NewPassword = req.body.NewPassword;
-  Corporate.findOneAndUpdate(
-    { Username: Username },
-    { Password : NewPassword },
-    { new: true },
-    (error, data) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(data);
-        res.status(200).send("update done");
-      }
-    }
-  );
-  
-});
 module.exports = appRouter;
